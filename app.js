43d9eb10bc8c8661c7e60b4d8d05e9fa88c0fe0d
mari703 <160247770+mari703@users.noreply.github.com>
2024-10-18 12:17:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartão =document.createElement("article")
+    cartão.className ='cartão'
+
+    
+}
